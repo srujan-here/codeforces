fdfbf57f05a6a26e38542a0f110f7e6e7337d430
Srujansujji1717 <puttasrujan20bsc74@iiitkottayam.ac.in>
2022-02-04 12:21:41 +0530

    domino pilling

--- codeforces/basic/5.domino_pilling.cpp
+++ codeforces/basic/5.domino_pilling.cpp
@@ -1,5 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int a,b;
+    cin >> a>>b;
+    int t=a*b;
+    if(t%2==0){
+        cout<<t/2<<endl;
+    }
+    else{
+        cout<<(t-1)/2<<endl;
+    }
 }
